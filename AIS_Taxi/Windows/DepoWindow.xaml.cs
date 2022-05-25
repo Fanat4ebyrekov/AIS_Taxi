c229378658543e27f5e6c5c9497f0b9d92fa8979
Nikita Simonov <Nikitasimonov345633@hotmail.com>
2022-05-25 20:27:52 +0300

    Добавьте файлы проекта.

---
+++ AIS_Taxi/Windows/DepoWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AIS_Taxi.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для DepoWindow.xaml
+    /// </summary>
+    public partial class DepoWindow : Window
+    {
+        public DepoWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
